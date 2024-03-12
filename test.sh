126dd7ba3f25a6a9192dd327fd23d3f8723c21fd
b1u3-22 <jiri.s.15243@gmail.com>
2024-03-12 19:38:17 +0100

    fix: fixed start script
    Replaced python with python3
    Fixed location of various files

--- test.sh
+++ test.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+tests_location="./IPP24-Parser-Tester"
+
 # Check for existence of python venv
 # If it doesn't exist, then create it
-if ! test -d ./venv; then
-  python -m venv ./venv
-  . ./venv/bin/activate
-  pip3 install -r ./requirements.txt
+if ! test -d "${tests_location}/venv"; then
+  echo "Creating Python virtual environment"
+  python3 -m venv "${tests_location}/venv"
+  . "${tests_location}/venv/bin/activate"
+  echo "Installing required packages"
+  pip3 install -r "${tests_location}/requirements.txt"
   deactivate
 fi
 
 # Check for existence of parse.py script
-if ! test -f ../parse.py; then
+if ! test -f ./parse.py; then
   echo "Could not find the parse.py script. Exiting."
   exit 1
 fi
 
-.  ./venv/bin/activate        # Activate python venv
-clear                         # Clear terminal
-pytest ./ipp_parser_tests.py  # Start tests
-deactivate                    # Deactivate python venv
+.  "${tests_location}/venv/bin/activate" # Activate python venv
+clear
+cd "${tests_location}" || echo "Could not enter test directory" || exit 1 # Enter directory with tests
+pytest "./ipp_parser_tests.py"  # Start tests
+deactivate # Deactivate python venv
